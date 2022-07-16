60efbf82d78c352724f353a33c37e547f24be409
Denis Khokhlov <diffi1diffi@gmail.com>
2022-07-16 18:29:59 +0400

    Some changes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-test
+let uploadOverlay = document.querySelector('.img-upload__overlay');
+let body = document.body;
+let closeButton = document.querySelector('.img-upload__cancel');
+let uploadImgInput = document.querySelector('.img-upload__input');
+
+uploadImgInput.addEventListener("change", fileUploadChange);
+
+function fileUploadChange() {
+  uploadOverlay.classList.remove('hidden');
+  body.classList.add('modal-open');
+  let prevImgContainer = document.querySelector('.img-upload__preview');
+  prevImgContainer.querySelector('img').src = uploadImgInput.files[0].name;
+  console.log("ggggg");
+}
+
+closeButton.addEventListener("click", closeImgPrev);
+
+function closeImgPrev() {
+  uploadOverlay.classList.add('hidden');
+  body.classList.remove('modal-open');
+}
+
+document.addEventListener("keydown", escCloseImgPrev);
+
+function escCloseImgPrev(evt) {
+  if (evt.key === "Escape") {
+    closeImgPrev();
+    evt.preventDefault();
+  }
+}
+
+
