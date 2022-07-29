45f1a09263af214ebdbcfbe4c529c8b4cea91d4a
Denis Khokhlov <diffi1diffi@gmail.com>
2022-07-29 16:47:49 +0400

    исправление бага с задержкой отрисовки фотографий

--- js/posts-filter.js
+++ js/posts-filter.js
@@ -1,5 +1,5 @@
 import { createUsersPosts } from './social-images.js';
-import { shuffle } from './utils.js';
+import { shuffleArray } from './utils.js';
 import { debounce } from './utils.js';
 
 const filterElement = document.querySelector('.img-filters');
@@ -26,34 +26,28 @@ const changeFilter = (filterName) => {
   document.querySelector(`#${filterName}`).classList.add('img-filters__button--active');
 };
 
-const filterRandom = (userPosts) => shuffle(userPosts).slice(0, MAX_NUMBER_OF_RANDOM_POSTS);
+const filterRandom = (userPosts) => shuffleArray(userPosts).slice(0, MAX_NUMBER_OF_RANDOM_POSTS);
 
 const filterDiscuss = (userPosts) => userPosts.slice().sort((post1, post2) => post2.comments.length - post1.comments.length);
 
-const changePostsFilter = (evt, userPosts) => {
+const changePostsFilter = debounce((evt, userPosts) => {
   const filter = evt.target.id;
   deletePosts();
   switch (filter) {
     case 'filter-discussed':
-      debounce(() => {
-        changeFilter(filter);
-        createUsersPosts(filterDiscuss(userPosts));
-      }, DEBOUNCE_DELAY)();
+      changeFilter(filter);
+      createUsersPosts(filterDiscuss(userPosts));
       break;
     case 'filter-random':
-      debounce(() => {
-        changeFilter(filter);
-        createUsersPosts(filterRandom(userPosts, MAX_NUMBER_OF_RANDOM_POSTS));
-      }, DEBOUNCE_DELAY)();
+      changeFilter(filter);
+      createUsersPosts(filterRandom(userPosts, MAX_NUMBER_OF_RANDOM_POSTS));
       break;
     case 'filter-default':
-      debounce(() => {
-        changeFilter(filter);
-        createUsersPosts(userPosts);
-      }, DEBOUNCE_DELAY)();
+      changeFilter(filter);
+      createUsersPosts(userPosts);
       break;
   }
-};
+}, DEBOUNCE_DELAY);
 
 const enableFilter = () => {
   filterButton.forEach((button) => {
